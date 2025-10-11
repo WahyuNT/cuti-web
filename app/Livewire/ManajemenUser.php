52e493c94ef818789c9e94e4bcc5cdf1c13f6e55
WahyuNT <wahyuntx28@gmail.com>
2025-10-11 15:01:41 +0700

    update manajemen user

--- app/Livewire/ManajemenUser.php
+++ app/Livewire/ManajemenUser.php
@@ -3,11 +3,14 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 use Livewire\Component;
 
 class ManajemenUser extends Component
 {
+    public $title = 'Manajemen User';
     public $mode = 'view';
+    public $editId = null;
     public $name, $nip, $password, $role_id, $jabatan, $nomor_wa;
 
 
@@ -23,9 +26,11 @@ class ManajemenUser extends Component
     public function resetInput()
     {
         $this->mode = 'view';
+        $this->resetValidation();
         $this->reset(['name', 'nip', 'password', 'role_id', 'jabatan', 'nomor_wa']);
     }
-    public function create(){
+    public function create()
+    {
         $this->validate([
             'name' => 'required|string|max:255',
             'nip' => 'required|string|max:20|unique:users,nip',
@@ -35,19 +40,31 @@ class ManajemenUser extends Component
             'nomor_wa' => 'required|string|max:15',
         ]);
 
-        User::create([
-            'name' => $this->name,
-            'nip' => $this->nip,
-            'password' => bcrypt($this->password),
-            'role_id' => $this->role_id,
-            'jabatan' => $this->jabatan,
-            'nomor_wa' => $this->nomor_wa,
-        ]);
+        $user = new User();
+        $user->name = $this->name;
+        $user->nip = $this->nip;
+        $user->password = bcrypt($this->password);
+        $user->role_id = $this->role_id;
+        $user->jabatan = $this->jabatan;
+        $user->nomor_wa = $this->nomor_wa;
+        $user->save();
 
-        session()->flash('message', 'User berhasil ditambahkan.');
+        if ($user->save()) {
+            LivewireAlert::title('User Berhasil Dibuat!')
+                ->position('top-end')
+                ->toast()
+                ->success()
+                ->show();
+            $this->resetInput();
+        } else {
+            LivewireAlert::title('User Gagal Dibuat!')
+                ->position('top-end')
+                ->toast()
+                ->error()
+                ->show();
+        }
 
         $this->resetInput();
-
     }
     public function edit($id)
     {
@@ -58,6 +75,40 @@ class ManajemenUser extends Component
         $this->jabatan = $user->jabatan;
         $this->nomor_wa = $user->nomor_wa;
         $this->mode = 'edit';
-        // $this->editingUserId = $id;
+        $this->editId = $id;
+    }
+    public function update()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'nip' => 'required|string|max:20|unique:users,nip,' . $this->editId,
+            'role_id' => 'required|integer',
+            'jabatan' => 'required|integer|max:100',
+            'nomor_wa' => 'required|string|max:15',
+        ]);
+
+        $user = User::findOrFail($this->editId);
+        $user->name = $this->name;
+        $user->nip = $this->nip;
+        $user->role_id = $this->role_id;
+        $user->jabatan = $this->jabatan;
+        $user->nomor_wa = $this->nomor_wa;
+        $user->save();
+
+        if ($user->save()) {
+            LivewireAlert::title('User Berhasil Dieprbarui!')
+                ->position('top-end')
+                ->toast()
+                ->success()
+                ->show();
+
+            $this->resetInput();
+        } else {
+            LivewireAlert::title('User Gagal Dieprbarui!')
+                ->position('top-end')
+                ->toast()
+                ->error()
+                ->show();
+        }
     }
 }
--- resources/views/livewire/manajemen-user.blade.php
+++ resources/views/livewire/manajemen-user.blade.php
@@ -4,63 +4,65 @@
         <div class="w-full">
             <h2 class="text-2xl font-bold text-center">Manajemen User</h2>
         </div>
-        <div class="w-full grid grid-cols-2 gap-4 mt-4 items-end">
-            <div>
-                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">
-                    Cari Nama
-                </label>
-                <input type="text" id="first_name" placeholder="Nama User" required
-                    class="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
-            </div>
+        <div class="w-full">
+            <div class="w-full grid grid-cols-2 gap-4 mt-4 items-end">
 
-            <div class="flex justify-end items-end">
-                <button wire:click="toggleMode" type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none 
-        focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center hover:cursor-pointer">
-                    Tambah User
-                </button>
-            </div>
+                <div>
+                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">
+                        Cari Nama
+                    </label>
+                    <input type="text" id="first_name" placeholder="Nama User" required
+                        class="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                </div>
 
-        </div>
-        <div class="w-full mt-3 py-2 ">
-            <div class="relative overflow-x-auto rounded-xl border-1 border-gray-200">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-700  overflow-hidden">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-100 rounded-t-xl">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">Nama</th>
-                            <th scope="col" class="px-6 py-3">NIP</th>
-                            <th scope="col" class="px-6 py-3">Role</th>
-                            <th scope="col" class="px-6 py-3">Jabatan</th>
-                            <th scope="col" class="px-6 py-3">Nomor WA</th>
-                            <th scope="col" class="px-6 py-3 ">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($user as $item)
-                            <tr class="odd:bg-white even:bg-gray-50">
-                                <td class="px-6 py-4">{{ $item->name }}</td>
-                                <td class="px-6 py-4">{{ $item->nip }}</td>
-                                <td class="px-6 py-4">{{ $item->role_id }}</td>
-                                <td class="px-6 py-4">{{ $item->jabatan }}</td>
-                                <td class="px-6 py-4 ">{{ $item->nomor_wa }}</td>
-                                <td class="">
-                                    <button
-                                        class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none 
-                                    focus:ring-yellow-300 font-medium rounded-lg text-sm w-full sm:w-auto px-1.5 py-1.5 text-center hover:cursor-pointer">
-                                        <i class="fa-solid fa-pen"></i>
-                                    </button>
-                                    <button
-                                        class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none 
-                                    focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-1.5 py-1.5 text-center hover:cursor-pointer">
-                                        <i class="fa-solid fa-trash"></i>
-                                    </button>
-                                </td>
+                <div class="flex justify-end items-end">
+                    <button wire:click="toggleMode" type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center hover:cursor-pointer">
+                        Tambah User
+                    </button>
+                </div>
+
+            </div>
+            <div class="w-full mt-3 py-2 ">
+                <div class="relative overflow-x-auto rounded-xl border-1 border-gray-200">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-700  overflow-hidden">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-100 rounded-t-xl">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">Nama</th>
+                                <th scope="col" class="px-6 py-3">NIP</th>
+                                <th scope="col" class="px-6 py-3">Role</th>
+                                <th scope="col" class="px-6 py-3">Jabatan</th>
+                                <th scope="col" class="px-6 py-3">Nomor WA</th>
+                                <th scope="col" class="px-6 py-3 ">Aksi</th>
                             </tr>
-                        @empty
-                        @endforelse
+                        </thead>
+                        <tbody>
+                            @forelse ($user as $item)
+                                <tr class="odd:bg-white even:bg-gray-50">
+                                    <td class="px-6 py-4">{{ $item->name }}</td>
+                                    <td class="px-6 py-4">{{ $item->nip }}</td>
+                                    <td class="px-6 py-4">{{ $item->role_id }}</td>
+                                    <td class="px-6 py-4">{{ $item->jabatan }}</td>
+                                    <td class="px-6 py-4 ">{{ $item->nomor_wa }}</td>
+                                    <td class="">
+                                        <button wire:click="edit({{ $item->id }})"
+                                            class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none 
+                                    focus:ring-yellow-300 font-medium rounded-lg text-sm w-full sm:w-auto px-1.5 py-1.5 text-center hover:cursor-pointer">
+                                            <i class="fa-solid fa-pen"></i>
+                                        </button>
+                                        <button
+                                            class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none 
+                                    focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-1.5 py-1.5 text-center hover:cursor-pointer">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @empty
+                            @endforelse
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     @elseif($mode == 'edit')
@@ -142,11 +144,17 @@
                 </div>
 
                 <div class="col-span-2 flex justify-end items-end">
-                    <button wire:click="create" type="submit"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                        Simpan Perubahan
-                    </button>
-
+                    @if ($editId)
+                        <button wire:click="update" type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center hover:cursor-pointer">
+                            Simpan Perubahan
+                        </button>
+                    @else
+                        <button wire:click="update" type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center hover:cursor-pointer">
+                            Simpan User
+                        </button>
+                    @endif
                     <button type="button" wire:click="resetInput"
                         class="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ml-2 hover:cursor-pointer">
                         Batal
